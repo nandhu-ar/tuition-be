26627510871dc5a0dae2bcd6320042fd6a3a503f
nandhu-ar <nandhuaregi1996@gmail.com>
2024-04-13 15:23:00 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes.js';
+import cors from 'cors';
 
 var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
